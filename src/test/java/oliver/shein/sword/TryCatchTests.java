29469368c1b881f08bf3abf6e9c9a36ed69d21d5
cqu2015 <286931986@qq.com>
2018-12-05 17:17:23 +0800

    java类型强转

--- src/test/java/oliver/shein/sword/TryCatchTests.java
+++ src/test/java/oliver/shein/sword/TryCatchTests.java
@@ -1,22 +1,30 @@
 package oliver.shein.sword;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @Author Oliver Wang
  * @Description
  * @Created by IntelliJ IDEA 2018.
  * @Date Create at 2018/12/3 10:28
  */
+@Slf4j
 public class TryCatchTests {
     public static void main(String[] args) {
         try {
             int i = 100/0;
             System.out.println(i);
         }catch (ArithmeticException exc){
-            System.out.println("除数不能为0");
-            throw exc;
+            log.error("获取信息报错{}",exc);
+            System.out.println();
+            System.out.println("+++++++++++++++++++++++");
+            log.error("获取信息报错{}",exc.toString());
+            System.out.println(exc);
+            System.out.println("---------------");
         }catch (Exception ex){
             System.out.println("下面捕获了上面的异常"+ex.getMessage());
         }
+
         System.out.println("最后的语句");
     }
 }
