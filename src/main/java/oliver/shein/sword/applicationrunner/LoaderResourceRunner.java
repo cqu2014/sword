543a33721786e0c9718aa6c66a4b0c48ee58219f
oliver.wang@shein.com <286931986@qq.com>
2020-08-13 16:29:53 +0800

    RequestContextHolder

---
+++ src/main/java/oliver/shein/sword/applicationrunner/LoaderResourceRunner.java
@@ -0,0 +1,19 @@
+package oliver.shein.sword.applicationrunner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+/**
+ * @author Oliver Wang
+ * @description
+ * @created by IntelliJ IDEA 2018.3.3
+ * @date Create at 2020/4/27
+ * @since
+ */
+public class LoaderResourceRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
